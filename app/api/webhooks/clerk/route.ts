c7e3c587d7113a492c25871c67ed416315cd5217
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-02-21 21:28:59 +0200

    chore - .env

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -63,6 +63,7 @@ export async function POST(req: Request) {
 
   // CREATE
   if (eventType === "user.created") {
+    console.log("CREATED")
     const { id, email_addresses, image_url, first_name, last_name, username } =
       evt.data;
 
