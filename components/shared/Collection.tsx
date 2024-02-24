ab84283d4fd1f7ab1e9359d9574888674664b0d5
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-02-24 21:11:28 +0200

    chore - page.tsx ui

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -5,94 +5,92 @@ import Link from "next/link";
 import { useSearchParams, useRouter } from "next/navigation";
 import { CldImage } from "next-cloudinary";
 
-// import {
-//   Pagination,
-//   PaginationContent,
-//   PaginationNext,
-//   PaginationPrevious,
-// } from "@/components/ui/pagination";
-// import { transformationTypes } from "@/constants";
-// import { formUrlQuery } from "@/lib/utils";
-import Image as IImage from "../../lib/database/models/image.modal";
-
+import {
+  Pagination,
+  PaginationContent,
+  PaginationNext,
+  PaginationPrevious,
+} from "../ui/pagination";
+import { IImage } from "../../lib/database/models/image.modal";
 import { Button } from "../ui/button";
-
 import { Search } from "./Search";
+import { formUrlQuery } from "../../lib/utils";
+import { transformationTypes } from "../../constants";
 
-// export const Collection = ({
-//   hasSearch = false,
-//   images,
-//   totalPages = 1,
-//   page,
-// }: {
-//   images: IImage[];
-//   totalPages?: number;
-//   page: number;
-//   hasSearch?: boolean;
-// }) => {
-//   const router = useRouter();
-//   const searchParams = useSearchParams();
+export const Collection = ({
+  hasSearch = false,
+  images,
+  totalPages = 1,
+  page,
+}: {
+  images: IImage[];
+  totalPages?: number;
+  page: number;
+  hasSearch?: boolean;
+}) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
 
-//   // PAGINATION HANDLER
-//   const onPageChange = (action: string) => {
-//     const pageValue = action === "next" ? Number(page) + 1 : Number(page) - 1;
+  // PAGINATION HANDLER
+  const onPageChange = (action: string) => {
+    const pageValue = action === "next" ? Number(page) + 1 : Number(page) - 1;
 
-//     const newUrl = formUrlQuery({
-//       searchParams: searchParams.toString(),
-//       key: "page",
-//       value: pageValue,
-//     });
+    const newUrl = formUrlQuery({
+      searchParams: searchParams.toString(),
+      key: "page",
+      value: pageValue,
+    });
 
-//     router.push(newUrl, { scroll: false });
-//   };
+    router.push(newUrl, { scroll: false });
+  };
 
-//   return (
-//     <>
-//       <div className="collection-heading">
-//         <h2 className="h2-bold text-dark-600">Recent Edits</h2>
-//         {hasSearch && <Search />}
-//       </div>
+  return (
+    <>
+      <div className="collection-heading">
+        <h2 className="h2-bold text-dark-600">Recent Edits</h2>
+        {hasSearch && <Search />}
+      </div>
 
-//       {images.length > 0 ? (
-//         <ul className="collection-list">
-//           {images.map((image) => (
-//             <Card image={image} key={image._id} />
-//           ))}
-//         </ul>
-//       ) : (
-//         <div className="collection-empty">
-//           <p className="p-20-semibold">Empty List</p>
-//         </div>
-//       )}
+      {images.length > 0 ? (
+        <ul className="collection-list">
+          {images.map((image) => (
+            <Card image={image} key={image._id} />
+          ))}
+        </ul>
+      ) : (
+        <div className="collection-empty">
+          <p className="p-20-semibold">Empty List</p>
+        </div>
+      )}
 
-//       {totalPages > 1 && (
-//         <Pagination className="mt-10">
-//           <PaginationContent className="flex w-full">
-//             <Button
-//               disabled={Number(page) <= 1}
-//               className="collection-btn"
-//               onClick={() => onPageChange("prev")}
-//             >
-//               <PaginationPrevious className="hover:bg-transparent hover:text-white" />
-//             </Button>
+      {totalPages > 1 && (
+        <Pagination className="mt-10">
+          <PaginationContent className="flex w-full">
+            <Button
+              disabled={Number(page) <= 1}
+              className="collection-btn"
+              onClick={() => onPageChange("prev")}
+            >
+              <PaginationPrevious className="hover:bg-transparent hover:text-white" />
+            </Button>
 
-//             <p className="flex-center p-16-medium w-fit flex-1">
-//               {page} / {totalPages}
-//             </p>
+            <p className="flex-center p-16-medium w-fit flex-1">
+              {page} / {totalPages}
+            </p>
 
-//             <Button
-//               className="button w-32 bg-purple-gradient bg-cover text-white"
-//               onClick={() => onPageChange("next")}
-//               disabled={Number(page) >= totalPages}
-//             >
-//               <PaginationNext className="hover:bg-transparent hover:text-white" />
-//             </Button>
-//           </PaginationContent>
-//         </Pagination>
-//       )}
-//     </>
-//   );
-// };
+            <Button
+              className="button w-32 bg-purple-gradient bg-cover text-white"
+              onClick={() => onPageChange("next")}
+              disabled={Number(page) >= totalPages}
+            >
+              <PaginationNext className="hover:bg-transparent hover:text-white" />
+            </Button>
+          </PaginationContent>
+        </Pagination>
+      )}
+    </>
+  );
+};
 
 const Card = ({ image }: { image: IImage }) => {
   return (
