0ddc27100090492f964dcc9c5540b538377f87e0
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-02-24 21:51:30 +0200

    chore - download image

--- components/shared/TransformedImage.tsx
+++ components/shared/TransformedImage.tsx
@@ -1,8 +1,8 @@
-"use client"
-import { CldImage } from "next-cloudinary";
+"use client";
+import { CldImage, getCldImageUrl } from "next-cloudinary";
 import Image from "next/image";
 import React from "react";
-import { dataUrl, debounce, getImageSize } from "../../lib/utils";
+import { dataUrl, debounce, download, getImageSize } from "../../lib/utils";
 import { PlaceholderValue } from "next/dist/shared/lib/get-img-props";
 
 export const TransformedImage = ({
@@ -14,7 +14,21 @@ export const TransformedImage = ({
   setIsTransforming,
   hasDownload = false,
 }: TransformedImageProps) => {
-  const downloadHandler = () => {};
+  const downloadHandler = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    download(
+      getCldImageUrl({
+        width: image?.width,
+        height: image?.height,
+        src: image?.publicId,
+        ...transformationConfig,
+      }),
+      title
+    );
+  };
+
   return (
     <div className="flex flex-col gap-4">
       <div className="flex-between">
