0ff60930a00d2c071cb87efa39b13d1c39aa6299
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-02-24 21:42:58 +0200

    chore - page feed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,8 +3,15 @@ import React from "react";
 import { navLinks } from "../../constants";
 import Link from "next/link";
 import Image from "next/image";
+import { Collection } from "../../components/shared/Collection";
+import { getAllImages } from "../../lib/actions/image.actions";
 
-const Home = () => {
+const Home = async ({ searchParams }: SearchParamProps) => {
+  const page = Number(searchParams?.page) || 1;
+  const searchQuery = (searchParams?.query as string) || "";
+
+  const images = await getAllImages({ page, searchQuery });
+
   return (
     <>
       <section className="home">
@@ -25,6 +32,15 @@ const Home = () => {
             </Link>
           ))}
         </ul>
+
+        <section className="sm:mt-12">
+          <Collection
+            hasSearch
+            images={images?.data}
+            totalPages={images?.totalPage}
+            page={page}
+          />
+        </section>
       </section>
     </>
   );
